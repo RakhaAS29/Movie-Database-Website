5ca14277137389ed48990bf6d1b48a90ed0f73d8
Rakha A.S <rakha.amanta@gmail.com>
2026-02-15 10:46:57 +0700

    Force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;  
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS di production 
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
